f2cee7eacb9d52a618b2650f6a9b960b9b5fe6fa
aken <aken@kakao.com>
2023-05-06 02:56:39 +0900

    6. 작성글 저장 2 - 클래스 분리 (POST 엔티티 수정)

--- src/main/java/com/devlog/minu/api/domain/Post.java
+++ src/main/java/com/devlog/minu/api/domain/Post.java
@@ -6,20 +6,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor
 public class Post {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   private String title;
-
   @Lob
   private String content;
+
+  @Builder
+  public Post(String title, String content){
+    this.title = title;
+    this.content = content;
+  }
 
   public Post toEntity(PostCreate postCreate){
     this.title = postCreate.getTitle();
