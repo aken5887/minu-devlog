8fa4bfc0147edf73d6b811a79c30f39fc0d1377a
aken <aken@kakao.com>
2023-05-16 19:25:56 +0900

    22. CORS 문제 해결 - 서버 프록시 사용

--- src/main/java/com/devlog/minu/api/config/WebMVCConfig.java
+++ src/main/java/com/devlog/minu/api/config/WebMVCConfig.java
@@ -1,14 +1,13 @@
 package com.devlog.minu.api.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000");
-  }
+//  @Override
+//  public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//        .allowedOrigins("http://localhost:3000");
+//  }
 }
