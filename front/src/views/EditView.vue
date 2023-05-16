1ffcb63b46d02445908413d40a5fe25666a62b01
aken <aken@kakao.com>
2023-05-16 21:29:52 +0900

    25. 글 수정 화면 만들기

---
+++ front/src/views/EditView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+import {useRouter} from "vue-router";
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const router = useRouter();
+const post = ref({});
+const props = defineProps({
+  postId : {
+    type : [String, Number],
+    required : true
+  }
+});
+
+onMounted(() => {
+  axios.get(`/api/posts/${props.postId}`).then((response) => {
+    post.value = response.data;
+  });
+});
+
+const edit = function() {
+  axios.patch(`/api/posts/${props.postId}`, post.value).then((response)=>{
+    router.replace({name:"home"});
+  });
+}
+
+</script>
+
+<template>
+  <div>
+    <el-input v-model="post.title" placeholder="제목을 입력해주세요."/>
+  </div>
+  <div class="mt-2">
+    <el-input v-model="post.content" type="textarea" rows="15">
+    </el-input>
+  </div>
+  <div class="mt-2">
+    <el-button type="warning" @click="edit()">수정완료</el-button>
+  </div>
+</template>
+
+<style>
+</style>
--- front/src/views/ReadView.vue
+++ front/src/views/ReadView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
 
 const props = defineProps({
   postId : {
@@ -10,15 +11,21 @@ const props = defineProps({
 });
 
 const post = ref({});
+const router = useRouter();
 
 onMounted(() => {
   axios.get(`/api/posts/${props.postId}`).then((response) => {
       post.value = response.data;
     });
 });
+
+const moveToEdit = function() {
+  router.push({name : 'edit', params : { postId : props.postId }});
+}
 </script>
 
 <template>
   <h2>제목 : {{ post.title }}</h2>
   <div>내용 : {{ post.content }}</div>
+  <el-button type="warning" @click="moveToEdit()">수정</el-button>
 </template>
