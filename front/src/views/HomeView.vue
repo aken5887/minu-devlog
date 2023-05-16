1bb49fa835165ecd10fdad8ec03743537051ad7f
aken <aken@kakao.com>
2023-05-16 19:58:04 +0900

    23. 글 리스트 화면 만들기

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import axios from "axios";
+
+const posts = ref([]);
+
+  axios.get("/api/posts?page=1&size=5").then((response) => {
+    response.data.forEach((r: any) => {
+      posts.value.push(r);
+    });
+  });
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <div>
+        {{ post.title }}
+      </div>
+      <div>
+        {{ post.content }}
+      </div>
+    </li>
+  </ul>
 </template>
+
+<style>
+  li {
+    margin-bottom : 0.5em;
+  }
+  li:last-child {
+    margin-bottom : 0em;
+  }
+</style>
